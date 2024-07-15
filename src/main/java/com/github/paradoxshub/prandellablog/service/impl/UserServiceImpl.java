bf5566eca9f94467bdba57fd7e328f51205c4fdc
wewillnevermeet <2297555219@qq.com>
2024-07-15 12:32:55 +0800

    add new body parameter "nickname"——commit again

--- src/main/java/com/github/paradoxshub/prandellablog/service/impl/UserServiceImpl.java
+++ src/main/java/com/github/paradoxshub/prandellablog/service/impl/UserServiceImpl.java
@@ -37,6 +37,7 @@ public class UserServiceImpl implements UserService {
         System.out.println(count);
         return new InsertUserOutput(user.getId());
     }
+    //在加一个注释重新提交
 // 加个注释重新提交
 //    public List<User> updateUser() {
 //        return userMapper.updateUser();
